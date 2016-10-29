de7d5d0358076c22f1dea39da9e628ec744dcb7f
Noriaki Takasugi <takasugi@jsk.imi.i.u-tokyo.ac.jp>
2016-10-30 04:17:43 +0900

    Make Wrench Distributor

---
+++ rtc/AutoBalancer/WrenchDistributor.cpp
@@ -0,0 +1,192 @@
+/* -*- coding:utf-8-unix; mode:c++; -*- */
+
+#include "WrenchDistributor.h"
+
+
+void EndEffectorParam::calcStateConstraintsMatrix(hrp::dmatrix& C)
+{
+    C = hrp::dmatrix::Zero(1,state_dim);
+    C.block(0,0,1,3) << e_vec(0), e_vec(1), e_vec(2);
+}
+
+void EndEffectorParam::calcFrictionConstraintsMatrix(hrp::dmatrix& C)
+{
+    if ( move_vec.norm() == 0 ){ // static
+        C = hrp::dmatrix::Zero(4, state_dim);
+        C.block(0,0,1,3) <<  1, 0, mu_vec(0);
+        C.block(1,0,1,3) << -1, 0, mu_vec(0);
+        C.block(2,0,1,3) <<  0, 1, mu_vec(0);
+        C.block(3,0,1,3) <<  0,-1, mu_vec(0);
+    } else if ( move_vec(2) == 0 ) { // sliding
+        move_vec.normalize();
+        C = hrp::dmatrix::Zero(4, state_dim);
+        C.block(0,0,1,3) <<  1, 0, -mu_vec(1)*move_vec(0);
+        C.block(1,0,1,3) << -1, 0,  mu_vec(1)*move_vec(0);
+        C.block(2,0,1,3) <<  0, 1, -mu_vec(1)*move_vec(1);
+        C.block(3,0,1,3) <<  0,-1,  mu_vec(1)*move_vec(1);
+    } else { // float
+        C = hrp::dmatrix::Zero(2 * state_dim, state_dim);
+        C.block(0,0,state_dim,state_dim) = hrp::dmatrix::Identity(state_dim,state_dim);
+        C.block(state_dim,0,state_dim,state_dim) = -hrp::dmatrix::Identity(state_dim,state_dim);
+    }
+}
+
+void EndEffectorParam::calcMomentumConstraintsMatrix(hrp::dmatrix& C)
+{
+    // spv = (dx+,dx-,dy+,dy-)
+    C = hrp::dmatrix::Zero(6 ,state_dim);
+    if ( state_dim == 6 ){ // TODO (line contact)
+        // Support Polygon Constraints (taux, tauy)
+        C.block(0,0,4,state_dim) <<
+            0,0,support_polygon_vec(2),-1,0,0,
+            0,0,support_polygon_vec(3), 1,0,0,
+            0,0,support_polygon_vec(0),0,-1,0,
+            0,0,support_polygon_vec(1),0,1,0;
+        // Rotation Slip Suppression (tauz)
+        C.block(4,0,2,state_dim) <<
+            0,0,mu_vec(2),0,0,1,
+            0,0,mu_vec(2),0,0,-1;
+    }
+}
+
+void EndEffectorParam::calcConstraintsMatrix()
+{
+    hrp::dmatrix C_state, C_friction, C_moment;
+    calcStateConstraintsMatrix(C_state);
+    calcFrictionConstraintsMatrix(C_friction);
+    calcMomentumConstraintsMatrix(C_moment);
+    size_t cs_dim = C_state.rows();
+    size_t cf_dim = C_friction.rows();
+    size_t cm_dim = C_moment.rows();
+    c_dim = cs_dim+cf_dim+cm_dim;
+    Cmat = hrp::dmatrix::Zero(c_dim, state_dim);
+    Cmat.block(0,0,cs_dim,state_dim) = C_state;
+    Cmat.block(cs_dim,0,cf_dim,state_dim) = C_friction;
+    Cmat.block(cs_dim+cf_dim,0,cm_dim,state_dim) = C_moment;
+        // World Frame => Local Frame
+        hrp::dmatrix Rmat(hrp::dmatrix::Zero(state_dim, state_dim));
+        Rmat.block(0,0,3,3) = Rmat.block(3,3,3,3) = ee_rot;
+        Cmat = Cmat * Rmat;
+}
+
+void WrenchDistributor::calcAugmentedConstraintsMatrix(std::map<std::string, EndEffectorParam>& eeparam_map)
+{
+    constraints_dim = 0;
+    states_dim = 0;
+    for ( std::map<std::string, EndEffectorParam>::iterator it = eeparam_map.begin(); it != eeparam_map.end(); it++ ){
+        it->second.calcConstraintsMatrix();
+        constraints_dim += it->second.c_dim;
+        states_dim += it->second.state_dim;
+    }
+    weight_vector = hrp::dvector::Ones(6+states_dim);
+    weight_vector.head(6) = 1e5*hrp::dvector::Ones(6);
+    Amat = hrp::dmatrix::Zero(constraints_dim, states_dim);
+    size_t current_dim = 0;
+    size_t count = 0;
+    for ( std::map<std::string, EndEffectorParam>::iterator it = eeparam_map.begin(); it != eeparam_map.end(); it++ ){
+        weight_vector.segment( 6 + it->second.state_dim * count , it->second.state_dim ) << 1e-5, 1e-5, 1e-5, 1,1, 1;
+        weight_vector.segment( 6 + it->second.state_dim * count , it->second.state_dim ) *= it->second.weight;
+        Amat.block(current_dim, count*state_dim, it->second.c_dim, state_dim) = it->second.Cmat;
+        current_dim += it->second.c_dim;
+        count ++;
+    }
+}
+
+void WrenchDistributor::calcEvaluationFunctionMatrix(const std::map<std::string, EndEffectorParam>& eeparam_map)
+{
+    size_t Phirows = 6 + states_dim;
+    size_t Phicols = states_dim;
+    hrp::dmatrix Wmat(hrp::dmatrix::Zero(Phirows, Phirows));
+    hrp::dmatrix Ximat(hrp::dmatrix::Zero(Phirows, 1));
+    Phimat = hrp::dmatrix::Zero(Phirows, Phicols);
+    Wmat = weight_vector.asDiagonal();
+    Ximat.block(0,0,3,1) = ref_linear_momentum_rate + hrp::Vector3(0,0,mass*gravitational_acceleration);
+    Ximat.block(3,0,3,1) = ref_angular_momentum_rate;
+    size_t count = 0;
+    for ( std::map<std::string, EndEffectorParam>::const_iterator it = eeparam_map.begin(); it != eeparam_map.end(); it++ ){
+        Phimat.block(0, state_dim * count,3,3) = hrp::dmatrix::Identity(3,3);
+        Phimat.block(3,state_dim * count,3,3) <<
+                0,-(it->second.ee_pos(2)-ref_cog(2)),(it->second.ee_pos(1)-ref_cog(1)),
+                (it->second.ee_pos(2)-ref_cog(2)),0,-(it->second.ee_pos(0)-ref_cog(0)),
+            -(it->second.ee_pos(1)-ref_cog(1)),(it->second.ee_pos(0)-ref_cog(0)),0;
+        Phimat.block(3,state_dim * count+3,3,3) = hrp::dmatrix::Identity(3,3);
+        count ++;
+    }
+    Phimat.block(6,0,Phirows-6,Phicols) = hrp::dmatrix::Identity(Phirows-6,Phicols);
+    Hmat = Phimat.transpose()*Wmat*Phimat;
+    gvec = - Phimat.transpose()*Wmat*Ximat;
+}
+
+void WrenchDistributor::solveWrenchQP ()
+{
+    real_t* H = new real_t[states_dim*states_dim];
+    real_t* g = new real_t[states_dim];
+    real_t* A = new real_t[constraints_dim*states_dim];
+    real_t* lb = new real_t[states_dim];
+    real_t* ub = new real_t[states_dim];
+    real_t* lbA = new real_t[constraints_dim];
+    real_t* ubA = new real_t[constraints_dim];
+    for (size_t i = 0; i < states_dim; i++) {
+        for (size_t j = 0; j < states_dim; j++) {
+            H[i*states_dim+j] = Hmat(i,j);
+        }
+        g[i] = gvec(i);
+        lb[i] = -1e4;
+        ub[i] = 1e4;
+    }
+    for (size_t i = 0; i < constraints_dim; i++) {
+        for (size_t j = 0; j < states_dim; j++){
+            A[i*states_dim+j] = Amat(i,j);
+        }
+        lbA[i] = 0;
+        ubA[i] = 1e4;
+    }
+    SQProblem example( states_dim, constraints_dim );
+    Options options;
+    options.printLevel = PL_LOW;
+    example.setOptions( options );
+    // /* Solve first QP. */
+    int nWSR = 1e5;
+    example.init(H,g,A,lb,ub,lbA,ubA,nWSR);
+    real_t* xOpt = new real_t[states_dim];
+    example.getPrimalSolution( xOpt );
+    size_t count = 0;
+    wrenches.resize(states_dim);
+    for ( size_t i = 0; i < ee_num; i++ )
+        for ( size_t j = 0; j < state_dim; j ++)
+            wrenches(i*state_dim+j) = xOpt[i*state_dim+j];
+    delete[] H;
+    delete[] g;
+    delete[] A;
+    delete[] lb;
+    delete[] ub;
+    delete[] lbA;
+    delete[] ubA;
+    delete[] xOpt;
+};
+
+void WrenchDistributor::calcMomentumRate ()
+{
+    linear_momentum_rate = (Phimat * wrenches).segment(0,3) - hrp::Vector3(0,0,mass * gravitational_acceleration);
+    angular_momentum_rate = (Phimat * wrenches).segment(3,6);
+}
+
+void WrenchDistributor::printResult (const std::map<std::string, EndEffectorParam>& eeparam_map)
+{
+    std::cerr << "------------------------------------------" << std::endl;
+    std::cerr << "CoM" << std::endl;
+    std::cerr << ref_cog.transpose() << std::endl;
+    std::cerr << "------------------------------------------" << std::endl;
+    std::cerr << "reference linear momentum" << std::endl;
+    std::cerr << ref_linear_momentum_rate.transpose() << std::endl;
+    std::cerr << "reference angular momentum" << std::endl;
+    std::cerr << ref_angular_momentum_rate.transpose() << std::endl;
+    std::cerr << "------------------------------------------" << std::endl;
+    std::cerr << "linear momentum" << std::endl;
+    std::cerr << linear_momentum_rate.transpose() << std::endl;
+    std::cerr << "angluar momentum" << std::endl;
+    std::cerr << angular_momentum_rate.transpose() << std::endl;
+    std::cerr << "wrenches" << std::endl;
+    for ( std::map<std::string, EndEffectorParam>::const_iterator it = eeparam_map.begin(); it != eeparam_map.end(); it++ )
+        std::cerr << it->second.wrench.transpose() << std::endl;
+}
---
+++ rtc/AutoBalancer/testWrenchDistributor.cpp
@@ -0,0 +1,229 @@
+/* -*- coding:utf-8-unix; mode:c++; -*- */
+
+#include "WrenchDistributor.h"
+#include "PreviewController.h"
+
+using namespace hrp;
+using namespace rats;
+
+// calculate time
+#include <cstdio>
+#include <sys/time.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+struct __mtimer__ {
+  double start;
+  char msg[100];
+  bool is_sec_print;
+  __mtimer__(const char* format, ...)
+  __attribute__((format(printf, 2, 3)))
+  {
+    va_list args;
+    va_start(args, format);
+    vsnprintf(msg, sizeof(msg), format, args);
+    va_end(args);
+
+    start = sec();
+  }
+  ~__mtimer__() {
+    fprintf(stderr, "[%s] => %.6f [ms]\n", msg, 1000*(sec() - start));
+  }
+  double sec() {
+    struct timeval tv;
+    gettimeofday(&tv, NULL);
+    return tv.tv_sec + tv.tv_usec * 1e-6;
+  }
+  operator bool() { return false; }
+};
+
+#define mtimer(...) if(__mtimer__ __b__ = __mtimer__(__VA_ARGS__));else
+
+int main(int argc, char* argv[])
+{
+    //
+    bool use_gnuplot = true;
+    double dt = 0.04, max_tm = 10.0;
+    std::queue<hrp::Vector3> ref_zmp_list;
+    std::deque<double> tm_list;
+    //
+    double mass = 56;
+    double g = 9.8066;
+    double z_c = 0.8;
+    WrenchDistributor wd(mass, g);
+    //
+    for (size_t i = 0; i < static_cast<size_t>(round(max_tm / dt)); i++) {
+        double tmp_tm = i * dt;
+        tm_list.push_back(tmp_tm);
+        hrp::Vector3 v;
+        double step_time = 5;
+        double dspr = 0.2;
+        if (tmp_tm < step_time * dspr) { // dsp
+            v << 0, 0.1 * tmp_tm / (step_time * dspr) , 0;
+        } else if (tmp_tm < step_time * (1 - dspr)) { // ssp
+            v << 0, 0.1, 0;
+        } else if (tmp_tm < step_time * (1 + dspr)) { //dsp
+            v << 0.15 - 0.15 * (step_time * (1 + dspr) - tmp_tm) / ( 2 * step_time * dspr) , -0.1 + 0.2 * (step_time * (1 + dspr) - tmp_tm) / ( 2 * step_time * dspr), 0;
+        } else if (tmp_tm < step_time * (2 - dspr)) {
+            v << 0.15, -0.1, 0;
+        } else {
+            v << 0.15, (10 - tmp_tm) / ( step_time * dspr ) * -0.1, 0;
+        }
+        ref_zmp_list.push(v);
+    }
+    //
+    preview_dynamics_filter<extended_preview_control> df(dt, z_c, ref_zmp_list.front());
+    std::string fname("/tmp/plot.dat");
+    FILE* fp = fopen(fname.c_str(), "w");
+    double cart_zmp[3], refzmp[3];
+    bool r = true;
+    size_t index = 0;
+    // for eeparam
+    std::map<std::string, EndEffectorParam> eeparam_map;
+    EndEffectorParam rleg(hrp::Vector3(0, -0.1, 0), hrp::Matrix33::Identity());
+    EndEffectorParam lleg(hrp::Vector3(0,  0.1, 0), hrp::Matrix33::Identity());
+    eeparam_map.insert(std::pair<std::string, EndEffectorParam>("rleg", rleg));
+    eeparam_map.insert(std::pair<std::string, EndEffectorParam>("lleg", lleg));
+    while (r) {
+        hrp::Vector3 p, x; // x cog
+        std::vector<hrp::Vector3> qdata;
+        r = df.update(p, x, qdata, ref_zmp_list.front(), qdata, !ref_zmp_list.empty());
+        if (r) {
+            index++;
+            df.get_cart_zmp(cart_zmp);
+            df.get_current_refzmp(refzmp);
+            // wrench distributor
+            std::map<std::string, EndEffectorParam> tmp_eeparam_map;
+            double ratio;
+            //// reference dynamics
+            double omega2 = g / z_c;
+            hrp::Vector3 com_pos = hrp::Vector3(x[0],x[1],0.8);
+            hrp::Vector3 ref_linear_momentum_rate = mass * hrp::Vector3(omega2*(x[0]-cart_zmp[0]),omega2*(x[1]-cart_zmp[1]),0);
+            hrp::Vector3 ref_angular_momentum_rate = hrp::Vector3(0,0,0);
+            //// contact states
+            if ( index < 25 ) {
+                tmp_eeparam_map["rleg"] = eeparam_map["rleg"];
+                tmp_eeparam_map["lleg"] = eeparam_map["lleg"];
+            } else if ( index < 100 ){
+                tmp_eeparam_map["lleg"] = eeparam_map["lleg"];
+            } else if ( index < 150 ){
+                eeparam_map["rleg"].setEEParam(hrp::Vector3(0.15,-0.1,0.0), hrp::Matrix33::Identity());
+                tmp_eeparam_map["rleg"] = eeparam_map["rleg"];
+                tmp_eeparam_map["lleg"] = eeparam_map["lleg"];
+            } else if (index < 225 ){
+                tmp_eeparam_map["rleg"] = eeparam_map["rleg"];
+            } else {
+                eeparam_map["lleg"].setEEParam(hrp::Vector3(0.15,0.1,0.0), hrp::Matrix33::Identity());
+                tmp_eeparam_map["rleg"] = eeparam_map["rleg"];
+                tmp_eeparam_map["lleg"] = eeparam_map["lleg"];
+            }
+            mtimer("time"){
+                wd.DistributeWrench(com_pos, ref_linear_momentum_rate, ref_angular_momentum_rate, tmp_eeparam_map);
+            }
+            for ( std::map<std::string, EndEffectorParam>::iterator it = eeparam_map.begin(); it != eeparam_map.end(); it++ )
+                it->second.wrench = hrp::dvector::Zero(it->second.state_dim);
+            for ( std::map<std::string, EndEffectorParam>::iterator it = tmp_eeparam_map.begin(); it != tmp_eeparam_map.end(); it++ )
+                eeparam_map[it->first] = it->second;
+            // wd.printResult(eeparam_map);
+            std::cerr << "at " << index * dt << " [s]"<< std::endl;
+            // print
+            fprintf(fp, "%f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f\n",
+                    tm_list[index],
+                    cart_zmp[0], /* zmpx ;; this zmp is "zmp as a table-cart model" */
+                    x[0], /* cogy */
+                    refzmp[0], /* refzmpx */
+                    cart_zmp[1], /* zmpy ;; this zmp is "zmp as a table-cart model" */
+                    x[1], /* cogy */
+                    refzmp[1], /* refzmpy */
+                    ref_linear_momentum_rate[0] / mass, // reference cog_acc x
+                    wd.linear_momentum_rate[0] / mass, // cog_acc x
+                    ref_linear_momentum_rate[1] / mass, // reference cog_acc y
+                    wd.linear_momentum_rate[1] / mass, // cog_acc y
+                    ref_linear_momentum_rate[2] / mass, // reference cog_acc y
+                    wd.linear_momentum_rate[2] / mass, // cog_acc y
+                    ref_angular_momentum_rate[0], // reference angular momentum rate x
+                    wd.angular_momentum_rate[0], // angular momentum rate x
+                    ref_angular_momentum_rate[1], // reference angular momentum rate y
+                    wd.angular_momentum_rate[1], // angular momentum rate y
+                    ref_angular_momentum_rate[2], // reference angular momentum rate z
+                    wd.angular_momentum_rate[2], // angular momentum rate z
+                    eeparam_map["rleg"].wrench[0], // rleg force x
+                    eeparam_map["lleg"].wrench[0], // lleg force x
+                    eeparam_map["rleg"].wrench[1], // rleg force y
+                    eeparam_map["lleg"].wrench[1], // lleg force y
+                    eeparam_map["rleg"].wrench[2], // rleg force z
+                    eeparam_map["lleg"].wrench[2] // lleg force z
+                    );
+        } else if ( !ref_zmp_list.empty() ) r = true;
+        if (!ref_zmp_list.empty()) ref_zmp_list.pop();
+    }
+    fclose(fp);
+    if (use_gnuplot) {
+        FILE* gp[11];
+        std::string titles[] = {"com pos X", "com pos Y", "com acc X", "com acc Y", "com acc Z", "angular momentum rate x", "angular momentum rate y", "angular momentum rate z", "force x", "force y", "force z"};
+        for (size_t ii = 0; ii < 2; ii++) {
+            gp[ii] = popen("gnuplot", "w");
+            fprintf(gp[ii], "set title \"%s\"\n", titles[ii].c_str());
+            fprintf(gp[ii], "plot \"%s\" using 1:%zu with lines title \"cart-table zmp\"\n", fname.c_str(), ( ii * 3 + 2));
+            fprintf(gp[ii], "replot \"%s\" using 1:%zu with lines title \"cog\"\n", fname.c_str(), ( ii * 3 + 3));
+            fprintf(gp[ii], "replot \"%s\" using 1:%zu with lines title \"refzmp\"\n", fname.c_str(), ( ii * 3 + 4));
+            fflush(gp[ii]);
+        }
+        for (size_t ii = 0; ii < 3; ii++) {
+            gp[ii] = popen("gnuplot", "w");
+            fprintf(gp[ii], "set title \"%s\"\n", titles[2+ii].c_str());
+            fprintf(gp[ii], "plot \"%s\" using 1:%zu with lines title \"reference cog acc\"\n", fname.c_str(), ( ii * 2 + 8 ));
+            fprintf(gp[ii], "replot \"%s\" using 1:%zu with lines title \"cog acc\"\n", fname.c_str(), ( ii * 2 + 9 ));
+            fflush(gp[ii]);
+        }
+        for (size_t ii = 0; ii < 3; ii++) {
+            gp[ii] = popen("gnuplot", "w");
+            fprintf(gp[ii], "set title \"%s\"\n", titles[5+ii].c_str());
+            fprintf(gp[ii], "plot \"%s\" using 1:%zu with lines title \"reference angular momentum rate\"\n", fname.c_str(), ( ii * 2 + 14 ));
+            fprintf(gp[ii], "replot \"%s\" using 1:%zu with lines title \"angular momentum rate\"\n", fname.c_str(), ( ii * 2 + 15 ));
+            fflush(gp[ii]);
+        }
+        for (size_t ii = 0; ii < 3; ii++) {
+            gp[ii] = popen("gnuplot", "w");
+            fprintf(gp[ii], "set title \"%s\"\n", titles[8+ii].c_str());
+            fprintf(gp[ii], "plot \"%s\" using 1:%zu with lines title \"rleg force\"\n", fname.c_str(), ( ii * 2 + 20 ));
+            fprintf(gp[ii], "replot \"%s\" using 1:%zu with lines title \"lleg force\"\n", fname.c_str(), ( ii * 2 + 21 ));
+            fflush(gp[ii]);
+        }
+        double tmp;
+        std::cin >> tmp;
+        for (size_t j = 0; j < 8; j++) pclose(gp[j]);
+    }
+
+    // // Initialization
+    // double mass = 56;
+    // double g = 9.8066;
+    // WrenchDistributor wd(mass, g);
+
+    // // Proc One Tic
+    // //// End Effector
+    // size_t ee_num = 2;
+    // std::vector<EndEffectorParam> eeparam_vec;
+    // eeparam_vec.resize(ee_num);
+    // //// Constraints
+    // eeparam_vec[0].setEEParam(hrp::Vector3(0,-0.105,0), hrp::Matrix33::Identity());
+    // eeparam_vec[1].setEEParam(hrp::Vector3(0,0.105,0), hrp::Matrix33::Identity());
+    // // eeparam_vec[2].setEEParam(hrp::Vector3(0.2,-0.105,0.5), hrp::Matrix33::Identity());
+    // // eeparam_vec[3].setEEParam(hrp::Vector3(0.2,0.105,0.5), hrp::Matrix33::Identity());
+    // for ( std::vector<EndEffectorParam>::iterator it = eeparam_vec.begin(); it != eeparam_vec.end(); it++ ){
+    //     it->calcConstraintsMatrix();
+    // }
+    // ///// Reference Dynamics
+    // hrp::Vector3 com_pos = hrp::Vector3(0.2,0,0.7);
+    // hrp::Vector3 ref_lmr = hrp::Vector3(0.1,0,0);
+    // hrp::Vector3 ref_amr = hrp::Vector3(0,0,0);
+    // //// Wegiht
+    // hrp::dvector weight_vector(1e-5*hrp::dvector::Ones(6*ee_num+6));
+    // weight_vector.head(6) = hrp::dvector::Ones(6);
+    // ////
+    // wd.setParam(com_pos, ref_lmr, ref_amr, ee_num, eeparam_vec, weight_vector);
+    // mtimer("time") {
+    //     wd.solveWrenchQP();
+    // }
+    // wd.printResult();
+}
